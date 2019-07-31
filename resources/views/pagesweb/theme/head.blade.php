4ca1debb5ac5482625561f34377217717927b8ca
Mirna Valdivia <miirna.valdiiviia@gmail.com>
2019-07-31 13:49:21 -0700

    agregando todos los estilo que se utilizaran, asi como creando los blades

---
+++ resources/views/pagesweb/theme/head.blade.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compartible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Smart Parking</title>
