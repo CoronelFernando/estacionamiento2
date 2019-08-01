3b13b93948a3a1bcb61ace1e2775b166a6050979
Mirna Valdivia <miirna.valdiiviia@gmail.com>
2019-08-01 14:48:52 -0700

    mas mas cambios

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,15 +4,12 @@
     @include('theme.head')
 </head>
 <body id="page-top">
-    <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <div id="wrapper">
     @include('theme.sidebar')
         <div id="content-wrapper" class="d-flex flex-column">
             <div id="content">
                 @include('theme.header')
                 <div class="container-fluid">
-                    <input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
-                    @section('content')
                     @yield('content')
                     <footer class="sticky-footer bg-white">
                     @include('theme.footer')
@@ -25,12 +22,12 @@
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-aungle-up"></i>
     </a>
-    <script src="{{url('style/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{url('style/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{url('style/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-    <script src="{{url('style/js/sb-admin-2.min.js')}}"></script>
-    <script src="{{url('style/vendor/chart.js/Chart.min.js')}}"></script>
-    <script src="{{url('style/js/demo/chart-area-demo.js')}}"></script>
-    <script src="{{url('style/js/demo/chart-pie-demo.js')}}"></script>
+    <script src="{{url('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{url('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{url('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <script src="{{url('js/sb-admin-2.min.js')}}"></script>
+    <script src="{{url('vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{url('js/demo/chart-area-demo.js')}}"></script>
+    <script src="{{url('js/demo/chart-pie-demo.js')}}"></script>
 </body>
 </html>
--- resources/views/theme/head.blade.php
+++ resources/views/theme/head.blade.php
@@ -1,11 +1,14 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compartible" content="IE=edge">
+<meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="">
-<meta name="author" content="">
-
-<title>Smart Parking</title>
-
-<link href="{{url('style/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-<link href="http://fonts.googleapis.com/css?family=Nunito:200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" type="text/css">
-<link href="{{url('style/css/sb-admin-2.css')}}" rel="styleshhet">
+  <title>
+    Argon Dashboard - Free Dashboard for Bootstrap 4 by Creative Tim
+  </title>
+  <!-- Favicon -->
+  <link href="{{url('img/brand/favicon.png')}}" rel="icon" type="image/png">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <!-- Icons -->
+  <link href="{{url('js/plugins/nucleo/css/nucleo.css')}}" rel="stylesheet" />
+  <link href="{{url('js/plugins/@fortawesome/fontawesome-free/css/all.min.css')}}" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="{{url('css/argon-dashboard.css?v=1.1.0')}}" rel="stylesheet" />
